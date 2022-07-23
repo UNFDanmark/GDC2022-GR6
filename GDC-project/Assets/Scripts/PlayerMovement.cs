5a0ca39836d293b6bdabfa107028e0d41d5bf43c
UNF-GDC <109671887+UNF-GDC@users.noreply.github.com>
2022-07-23 17:59:06 +0200

    Added basic movement

---
+++ GDC-project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed = 5f;
+    private Rigidbody rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalMovement = Input.GetAxis("Horizontal");
+        float verticalMovement = Input.GetAxis("Vertical");
+        Vector3 moveVector = new(horizontalMovement, rigidbody.velocity.y, verticalMovement);
+        rigidbody.velocity = moveVector * movementSpeed;
+    }
+}
