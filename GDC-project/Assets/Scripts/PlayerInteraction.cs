db78c01947d31ebc218a350a92557ceec7051d4f
UNF-GDC <109671887+UNF-GDC@users.noreply.github.com>
2022-07-25 21:34:41 +0200

    Script cleanup

--- GDC-project/Assets/Scripts/PlayerInteraction.cs
+++ GDC-project/Assets/Scripts/PlayerInteraction.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerInteraction : MonoBehaviour
 {
     public int rayLength = 1;
+    public GameObject inventoryUI;
+    public Image keyImage;
 
     // Start is called before the first frame update
     void Start()
@@ -25,6 +28,10 @@ public class PlayerInteraction : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.E) && Physics.Raycast(transform.position, transform.forward, out hit, rayLength, LayerMask.GetMask("InteractableObject")))
         {
             hit.collider.gameObject.SendMessage("Interaction");
+        }
+        if (Input.GetKeyDown(KeyCode.E) && Physics.Raycast(transform.position, transform.forward, out hit, rayLength, LayerMask.GetMask("TransitionPoint")))
+        {
+            hit.collider.gameObject.SendMessage("Interaction", gameObject);
         }
         //print($"Interacted with item: {hit.collider.name}");
         //InteractableObject otherScript = hit.collider.gameObject.GetComponent<InteractableObject>();
